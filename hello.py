d6814b880223fd3f62b40c412660a92f00957d3d
dumartins <eduardo-s-martins@uol.com.br>
2025-09-19 21:01:51 -0400

    imported bootstrap and added base template

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -8,6 +11,10 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e): 
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
